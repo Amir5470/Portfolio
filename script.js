90f174ad1c60b8f60db9954613d99477ec881d80
Amir Mechkour <amirmechkour5474@gmail.com>
2026-01-10 22:55:00 -0500

    01/10/26 commit, Images
    Replaced Expired image links with permanent versions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const sunFrame = "https://s5.ezgif.com/tmp/ezgif-562cb55acebb80b3-gif-im/frame_00_delay-0.17s.gif";
-const moonFrame = "https://s5.ezgif.com/tmp/ezgif-562cb55acebb80b3-gif-im/frame_11_delay-0.05s.gif";
+const sunFrame = "/images/day-light.png";
+const moonFrame = "/images/night-dark.png";
 
-const forwardGIF = "https://s5.ezgif.com/tmp/ezgif-529eb59f51b64c81.gif";
-const reverseGIF = "https://s5.ezgif.com/tmp/ezgif-54267a40b7801651.gif";
+const forwardGIF = "/images/suntomoon.gif";
+const reverseGIF = "/images/moontosun.gif";
 
 const icon = document.getElementById("themeIcon");
 const toggle = document.getElementById("themeToggle");
